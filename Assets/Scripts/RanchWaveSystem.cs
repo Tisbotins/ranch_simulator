24102439d7e84b465f335ee6b6fc6c5b4c077fc2
Tejas Rangarajan <tisbo@mac.mynetworksettings.com>
2026-06-21 21:22:37 -0400

    Reduce enemy wave interval to one minute
    Set the first-wave, between-wave, and post-load countdowns to 60 seconds.

--- Assets/Scripts/RanchWaveSystem.cs
+++ Assets/Scripts/RanchWaveSystem.cs
@@ -16,8 +16,8 @@ public class RanchWaveSystem : MonoBehaviour
     public int CurrentWave { get; private set; }
     public int HighestWaveCleared { get; private set; }
     public WaveState CurrentState { get; private set; }
-    public float FirstWaveDelay = 120f;
-    public float IntermissionDuration = 120f;
+    public float FirstWaveDelay = 60f;
+    public float IntermissionDuration = 60f;
     public float EnemySpawnDelay = 0.75f;
     public int MaximumEnemiesPerWave = 30;
 
